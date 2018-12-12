ee2896f9dc66d311f12678d4377f568450a32be8
tharinduc <tharinduc@codegen.net>
2018-12-12 22:40:43 +0530

    added login service

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpService } from './http.service';
+import { HttpRequest } from '@angular/common/http';
 
 @Injectable()
 export class LoginService {
-  constructor(private loginService: LoginService) {
+  constructor(private httpService: HttpService) {
 
+  }
+
+  getAccessToken(grantType: string, clientId: string, clientSecret: string, authCode: string, redirectUri: string) {
+    const url = 'https://api.ciscospark.com/v1/access_token';
+    const method = 'POST';
+    const params = new URLSearchParams();
+    params.append('grant_type' , grantType);
+    params.append('client_id', clientId);
+    params.append('client_secret', clientSecret);
+    params.append('code', authCode);
+    params.append('redirect_uri', redirectUri);
+    const options = new HttpRequest(method, url, params);
+
+    return this.httpService.request(options);
   }
 }
