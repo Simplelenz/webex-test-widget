e58385b5cb7bfd490b147e858f6bd4eaf32bd1b3
tharinduc <tharinduc@codegen.net>
2018-12-19 12:47:25 +0530

    merge dev_v1.0 to DEV_tharindu

--- src/app/components/panel-container/panel-container.component.ts
+++ src/app/components/panel-container/panel-container.component.ts
@@ -1,9 +1,10 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {IconConstant} from '../../configurations/IconConstants';
-import {TAB} from '../navigation-bar/tabs.enum';
-import {RequestMethod, RequestOptions} from '@angular/http';
-import {URL} from '../../configurations/UrlConstants';
-import {HttpService} from '../../services/http.service';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild, ElementRef } from '@angular/core';
+import { IconConstant } from '../../configurations/IconConstants';
+import { TAB } from '../navigation-bar/tabs.enum';
+import { RequestMethod, RequestOptions } from '@angular/http';
+import { URL } from '../../configurations/UrlConstants';
+import { HttpService } from '../../services/http.service';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-panel-container',
@@ -16,12 +17,16 @@ export class PanelContainerComponent implements OnInit {
   @Input() email: string;
   @Output() close: EventEmitter<any> = new EventEmitter<any>();
 
+  videoElem: ElementRef;
+  viewVideoElem: ElementRef;
+  viewAudioElem: ElementRef;
+
   IconConstant: any = IconConstant;
   tab: any = TAB;
   panel = this.tab.CONVERSATION;
   showPopUp = false;
   showDeletePopUp = false;
-  visibleTabs = {CONVERSATION: true, CONTACTS: true, VIDEO: false, AUDIO: false, MESSAGE: false};
+  visibleTabs = { CONVERSATION: true, CONTACTS: true, VIDEO: false, AUDIO: false, MESSAGE: false };
   activatedTab = this.tab.CONVERSATION;
   contactList: any = [];
   conversationList: any = [];
@@ -31,14 +36,21 @@ export class PanelContainerComponent implements OnInit {
   showDoneButton = false;
   myDetails: any;
   selectedContacts: any = [];
+  spark: any;
+  constraints = {
+    audio: true,
+    video: false
+  };
 
-  constructor(private httpService: HttpService) {
+  constructor(private httpService: HttpService, private dataService: DataService) {
     if (this.contactList.length === 0) {
       this.getAllContacts();
     }
     if (this.conversationList.length === 0) {
       this.getAllConversations();
     }
+
+    this.spark = this.dataService.getSpark();
   }
 
   ngOnInit() {
@@ -56,7 +68,7 @@ export class PanelContainerComponent implements OnInit {
   clickDoneFunctionEmit(popUpData) {
     this.showPopUp = popUpData.popUp;
     if (popUpData.contactList.length === 1) {
-      this.visibleTabs = {CONVERSATION: false, CONTACTS: false, VIDEO: false, AUDIO: false, MESSAGE: true};
+      this.visibleTabs = { CONVERSATION: false, CONTACTS: false, VIDEO: false, AUDIO: false, MESSAGE: true };
       this.panel = this.tab.MESSAGE;
       this.activatedTab = this.tab.MESSAGE;
       this.contact = (popUpData.contactList[0]);
@@ -82,34 +94,35 @@ export class PanelContainerComponent implements OnInit {
       this.panel = this.activatedTab = this.tab.CONVERSATION;
     }
     if (this.panel === this.tab.VIDEO || this.panel === this.tab.AUDIO) {
-      this.visibleTabs = {CONVERSATION: false, CONTACTS: false, VIDEO: false, AUDIO: false, MESSAGE: true};
+      this.visibleTabs = { CONVERSATION: false, CONTACTS: false, VIDEO: false, AUDIO: false, MESSAGE: true };
       this.panel = this.activatedTab = this.tab.MESSAGE;
     }
   }
 
   clickCallFunctionEmit(type) {
     if (type === this.tab.VIDEO) {
-      this.visibleTabs = {CONVERSATION: false, CONTACTS: false, VIDEO: true, AUDIO: false, MESSAGE: false};
+      this.visibleTabs = { CONVERSATION: false, CONTACTS: false, VIDEO: true, AUDIO: false, MESSAGE: false };
       this.panel = this.activatedTab = this.tab.VIDEO;
     }
     if (type === this.tab.AUDIO) {
-      this.visibleTabs = {CONVERSATION: false, CONTACTS: false, VIDEO: false, AUDIO: true, MESSAGE: false};
+      this.visibleTabs = { CONVERSATION: false, CONTACTS: false, VIDEO: false, AUDIO: true, MESSAGE: false };
       this.panel = this.activatedTab = this.tab.AUDIO;
+      this.call();
     }
   }
 
   closeVideoCallFunctionEmit() {
-    this.visibleTabs = {CONVERSATION: false, CONTACTS: false, VIDEO: false, AUDIO: false, MESSAGE: true};
+    this.visibleTabs = { CONVERSATION: false, CONTACTS: false, VIDEO: false, AUDIO: false, MESSAGE: true };
     this.panel = this.activatedTab = this.tab.MESSAGE;
   }
 
   closeAudioCallFunctionEmit() {
-    this.visibleTabs = {CONVERSATION: false, CONTACTS: false, VIDEO: false, AUDIO: false, MESSAGE: true};
+    this.visibleTabs = { CONVERSATION: false, CONTACTS: false, VIDEO: false, AUDIO: false, MESSAGE: true };
     this.panel = this.activatedTab = this.tab.MESSAGE;
   }
 
   clickNewFunctionEmit() {
-    this.visibleTabs = {CONVERSATION: true, CONTACTS: true, VIDEO: false, AUDIO: false, MESSAGE: false};
+    this.visibleTabs = { CONVERSATION: true, CONTACTS: true, VIDEO: false, AUDIO: false, MESSAGE: false };
     this.panel = this.activatedTab = this.tab.CONTACTS;
   }
 
@@ -147,7 +160,7 @@ export class PanelContainerComponent implements OnInit {
     this.httpService.request(options).subscribe((response => {
       const temp: any = JSON.parse(response['_body']);
       this.conversation = temp.items;
-      this.visibleTabs = {CONVERSATION: false, CONTACTS: false, VIDEO: false, AUDIO: false, MESSAGE: true};
+      this.visibleTabs = { CONVERSATION: false, CONTACTS: false, VIDEO: false, AUDIO: false, MESSAGE: true };
       this.panel = this.activatedTab = this.tab.MESSAGE;
     }), error => {
       console.log(error);
@@ -248,6 +261,126 @@ export class PanelContainerComponent implements OnInit {
     }), error => {
       console.log(error);
     });
+  }
+
+  call() {
+    this.connect()
+      .then(() => {
+        const call = this.spark.phone.dial(this.contact.id, this.constraints);
+
+        // Call our helper function for binding events to calls
+        this.bindCallEvents(call);
+      });
+  }
+
+  bindCallEvents(call) {
+    // call is a call instance, not a promise, so to know if things break,
+    // we'll need to listen for the error event. Again, this is a rather naive
+    // handler.
+    call.on('error', (err) => {
+      console.error(err);
+      alert(err.stack);
+    });
+
+    // We can start rendering local and remote video before the call is
+    // officially connected but not right when it's dialed, so we'll need to
+    // listen for the streams to become available. We'll use `.once` instead
+    // of `.on` because those streams will not change for the duration of
+    // the call and it's one less event handler to worry about later.
+
+    call.once('localMediaStream:change', () => {
+      this.videoElem.nativeElement.srcObject = call.localMediaStream;
+    });
+
+    call.on('remoteMediaStream:change', () => {
+      // Ok, yea, this is a little weird. There's a Chrome behavior that prevents
+      // audio from playing from a video tag if there is no corresponding video
+      // track.
+      [
+        'audio',
+        'video'
+      ].forEach((kind) => {
+        if (call.remoteMediaStream) {
+          const track = call.remoteMediaStream.getTracks().find((t) => t.kind === kind);
+          if (track) {
+            if (kind === 'audio') {
+              this.viewAudioElem.nativeElement.srcObject = new MediaStream([track]);
+            }
+            if (kind === 'video') {
+              this.viewVideoElem.nativeElement.srcObject = new MediaStream([track]);
+            }
+          }
+        }
+      });
+    });
+
+    // Once the call ends, we'll want to clean up our UI a bit
+    call.on('inactive', () => {
+      // Remove the streams from the UI elements
+      this.videoElem.nativeElement.srcObject = undefined;
+      this.viewAudioElem.nativeElement.srcObject = undefined;
+      this.viewVideoElem.nativeElement.srcObject = undefined;
+    });
+
+    // Of course, we'd also like to be able to end the call:
+    document.getElementById('hangup').addEventListener('click', () => {
+      call.hangup();
+    });
+  }
+
+  connect() {
+    if (!this.spark.phone.registered) {
+      // we want to start listening for incoming calls *before* registering with
+      // the cloud so that we can join any calls that may already be in progress.
+      this.spark.phone.on('call:incoming', (call) => {
+        Promise.resolve()
+          .then(() => {
+            // Let's render the name of the person calling us. Note that calls
+            // from external sources (some SIP URIs, PSTN numbers, etc) may not
+            // have personIds, so we can't assume that field will exist.
+            if (call.from && call.from.personId) {
+              // In production, you'll want to cache this so you don't have to do
+              // a fetch on every incoming call.
+              return this.spark.people.get(call.from.personId);
+            }
+
+            return Promise.resolve();
+          })
+          .then((person) => {
+            const str = person ? `Anwser incoming call from ${person.displayName}` : 'Answer incoming call';
+            if (confirm(str)) {
+              call.answer();
+              this.bindCallEvents(call);
+            } else {
+              call.decline();
+            }
+          })
+          .catch((err) => {
+            console.error(err);
+            // alert(err);
+          });
+      });
+
+      return this.spark.phone.register()
+        .then(() => {
+          // This is just a little helper for our selenium tests and doesn't
+          // really matter for the example
+          // document.body.classList.add('listening');
+          console.log('phone registered');
+          // document.getElementById('connection-status').innerHTML = 'connected';
+        })
+        // This is a terrible way to handle errors, but anything more specific is
+        // going to depend a lot on your app
+        .catch((err) => {
+          console.error(err);
+          alert(err.stack);
+          // we'll rethrow here since we didn't really *handle* the error, we just
+          // reported it
+          throw err;
+        });
+    }
+
+    return Promise.resolve();
   }
 }
 
